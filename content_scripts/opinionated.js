1fead7fc7f03ca49d6a60a544ed6e0ef3edd0cb2
William Johnston <johnston.w@gmail.com>
2020-04-05 09:18:17 -0700

    checking urls on an interval

--- content_scripts/opinionated.js
+++ content_scripts/opinionated.js
@@ -5,13 +5,19 @@
         links.forEach((link) => {
             var url = link.href;
 
-            if (/opinions?\//.test(url)) {
+            if (/opinions?\//.test(url) && !/\[Opinion\]/.test(link.textContent)) {
                 link.textContent = '[Opinion] ' + link.textContent;
             }
         });
     }
 
+    setInterval(() => {
+        try {
+            find_links();
+        } catch (e) { }
+    }, 5000);
+
     try {
         find_links();
     } catch (e) { }
-})()
+})();
