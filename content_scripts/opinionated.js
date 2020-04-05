a878c64188dbab8e001bfa85e89c22917f84dbd5
William Johnston <johnston.w@gmail.com>
2020-04-05 16:59:22 -0700

    fixing for System.Object[] bump

--- content_scripts/opinionated.js
+++ content_scripts/opinionated.js
@@ -1,22 +1,48 @@
 (() => {
+    function findHeader(el, start) {
+        if(!start) {
+            start = 1
+        }
+
+        if (start > 6) {
+            return;
+        }
+
+        var h = el.querySelector('h' + start)
+
+        if(h) {
+           return h;
+        }
+
+        return findHeader(el, start+1);
+    }
+
     function find_links() {
         var links = document.querySelectorAll('a');
         var regex = /(?:opinions?|editorials?)(?:\/|\.|$)/i;
-        var location = window.location.href;
+        var loc = window.location.href;
 
-        if (regex.test(location)) {
+        loc = loc.split('?')[0]
+
+        if (regex.test(loc)) {
             return;
         }
 
         links.forEach((link) => {
-            var url = link.href;
+            var url = link.href.split('?')[0];
 
-            if (regex.test(url) && !/\[OPINIONATED\]/i.test(link.textContent)) {
+            if (regex.test(url) && !/(?:twitter\.com|facebook\.com)/i.test(url) && !/\[OPINIONATED\]/i.test(link.textContent) && !(/OPINION/i.test(link.textContent) || regex.test(link.textContent))) {
                 var el = document.createElement('span');
                 el.style.color = 'red';
                 el.style.fontWeight = 'bold';
                 el.textContent = '[OPINIONATED] ';
-                link.insertBefore(el, link.firstChild);
+                var header = findHeader(link);
+
+                if(header) {
+                    header.insertBefore(el, header.firstChild);
+                } else {
+                    link.insertBefore(el, link.firstChild);
+                }
             }
         });
     }
