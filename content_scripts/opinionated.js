1fa8a9e66d3c27cb47698debdd3abd5717385a1a
William Johnston <johnston.w@gmail.com>
2020-04-05 16:15:22 -0700

    adding System.Object[] opinion urls specifically

--- content_scripts/opinionated.js
+++ content_scripts/opinionated.js
@@ -1,12 +1,22 @@
 (() => {
     function find_links() {
         var links = document.querySelectorAll('a');
+        var regex = /(?:opinions?|editorials?)(?:\/|\.|$)/i;
+        var location = window.location.href;
+
+        if (regex.test(location)) {
+            return;
+        }
 
         links.forEach((link) => {
             var url = link.href;
 
-            if (/opinions?\//.test(url) && !/\[Opinionated\]/.test(link.textContent)) {
-                link.textContent = '[Opinionated] ' + link.textContent;
+            if (regex.test(url) && !/OPINION/i.test(link.textContent)) {
+                var el = document.createElement('span');
+                el.style.color = 'red';
+                el.style.fontWeight = 'bold';
+                el.textContent = '[OPINIONATED] ';
+                link.insertBefore(el, link.firstChild);
             }
         });
     }
