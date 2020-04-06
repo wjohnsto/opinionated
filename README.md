7dcae8e44c7e8ae2b228fe9d71610d0a8db61ff4
William Johnston <johnston.w@gmail.com>
2020-04-06 14:52:06 -0700

    including bias ratings

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-The purpose of this addon is to inform you when a link location is an opinion article. Most popular news sites have opinion blogs, and you don't want to confuse those articles with reporting articles. The addon will add "[Opinionated]" to the text content of every identified URL that links to an opinion article.
+The purpose of this addon is to inform you when a link location is an opinion article. Most popular news sites have opinion blogs, and you don't want to confuse those articles with reporting articles. The addon will add "[OPINIONATED]" to the text content of every identified URL that links to an opinion article.
+
+Alternatively if an opinion article is not linked, this addon will use a curated list from https://www.allsides.com to indicate the bias of a new source:
+
+1. "[<<]" = Left
+1. "[<]"  = Lean Left
+1. "[<>]" = Center
+1. "[>]"  = Lean Right
+1. "[>>]" = Right
--- content_scripts/opinionated.js
+++ content_scripts/opinionated.js
@@ -1,6 +1,17 @@
+
 (() => {
+    var ratings = window.biasRatings;
+    var ratingsKeys = Object.keys(ratings);
+    var ratingsMap = {
+        'Left': '<<',
+        'Lean Left': '<',
+        'Center': '<>',
+        'Lean Right': '>',
+        'Right': '>>'
+    };
+
     function findHeader(el, start) {
-        if(!start) {
+        if (!start) {
             start = 1
         }
 
@@ -10,16 +21,16 @@
 
         var h = el.querySelector('h' + start)
 
-        if(h) {
-           return h;
+        if (h) {
+            return h;
         }
 
-        return findHeader(el, start+1);
+        return findHeader(el, start + 1);
     }
 
     function find_links() {
         var links = document.querySelectorAll('a');
-        var regex = /(?:opinions?|editorials?)(?:\/|\.|$)/i;
+        var regex = /(?:opinions?|editorials?)(?:[^\/]*)\//i;
         var loc = window.location.href;
 
         loc = loc.split('?')[0]
@@ -28,17 +39,60 @@
             return;
         }
 
+        var locParts = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/i.exec(loc);
+        var host = locParts[2];
+
         links.forEach((link) => {
             var url = link.href.split('?')[0];
+            var rating = '';
+            var urlParts = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/i.exec(url);
 
-            if (regex.test(url) && !/(?:twitter\.com|facebook\.com)/i.test(url) && !/\[OPINIONATED\]/i.test(link.textContent) && !(/OPINION/i.test(link.textContent) || regex.test(link.textContent))) {
+            if (!urlParts) {
+                return;
+            }
+
+            var urlHost = urlParts[2];
+
+
+            if (urlHost === host) {
+                return;
+            }
+
+            for (var i = 0; i < ratingsKeys.length; ++i) {
+                if (url.toLowerCase().indexOf(ratingsKeys[i].toLowerCase()) > -1) {
+                    rating = ratingsMap[ratings[ratingsKeys[i]].rating] || '';
+                }
+            }
+
+            if (regex.test(url) && !/(?:twitter\.com|facebook\.com)/i.test(url)) {
+                if (!/\[OPINIONATED\]/i.test(link.textContent) && !(/OPINION/i.test(link.textContent) || regex.test(link.textContent))) {
+                    var el = document.createElement('span');
+                    el.style.color = 'red';
+                    el.style.fontWeight = 'bold';
+                    el.textContent = '[OPINIONATED] ';
+
+                    var header = findHeader(link);
+
+                    if (header) {
+                        header.insertBefore(el, header.firstChild);
+                    } else {
+                        link.insertBefore(el, link.firstChild);
+                    }
+                }
+            } else if (rating && link.textContent.indexOf(rating) === -1) {
                 var el = document.createElement('span');
                 el.style.color = 'red';
                 el.style.fontWeight = 'bold';
-                el.textContent = '[OPINIONATED] ';
+
+                if (rating) {
+
+                } else {
+
+                }
+                el.textContent = '[' + rating + '] ';
                 var header = findHeader(link);
 
-                if(header) {
+                if (header) {
                     header.insertBefore(el, header.firstChild);
                 } else {
                     link.insertBefore(el, link.firstChild);
